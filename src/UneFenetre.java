7d71f575cc2fa1f3e4d9579b5c71cf96b11ce073
Baptiste Fournie <22205554@me.uvsq.fr>
2024-09-20 15:46:23 +0200

    Initial commit

---
+++ src/UneFenetre.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
+class UneFenetre extends JFrame 
+{
+    UnMobile sonMobile;
+    private final int LARG=400, HAUT=250;
+    
+    public UneFenetre()
+    {
+	// TODO 
+	// ajouter sonMobile a la fenetre
+	// creer une thread laThread avec sonMobile
+	// afficher la fenetre
+	// lancer laThread 
+    }
+}
