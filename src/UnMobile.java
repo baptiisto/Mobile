bb442ddcecf7b48b8f7a022a83332df61f977ab2
baptiisto <baptisto78@gmail.com>
2024-10-04 14:01:13 +0200

    (feat) : enlevage du bouton qui marche pas

--- src/UnMobile.java
+++ src/UnMobile.java
@@ -5,7 +5,6 @@ class UnMobile extends JPanel implements Runnable
 {
     int saLargeur, saHauteur, sonDebDessin;
     final int sonPas = 10, sonTemps=50, sonCote=40;
-	private boolean running = true;
     
     UnMobile(int telleLargeur, int telleHauteur)
     {
@@ -15,28 +14,21 @@ class UnMobile extends JPanel implements Runnable
 	setSize(telleLargeur, telleHauteur);
     }
 
-    public void run() {
-		for (sonDebDessin = 0; sonDebDessin < saLargeur - sonPas; sonDebDessin += sonPas)
-			while (true) {
-				if(running){
-				{
-					repaint();
-					try {
-						Thread.sleep(sonTemps);
-					} catch (InterruptedException telleExcp) {
-						telleExcp.printStackTrace();
-					}
-				}
-				for (sonDebDessin = saLargeur; sonDebDessin > sonPas; sonDebDessin -= sonPas) {
-					repaint();
-					try {
-						Thread.sleep(sonTemps);
-					} catch (InterruptedException telleExcp) {
-						telleExcp.printStackTrace();
-					}
-				}
-				}
-			}
+    public void run()
+    {
+	for (sonDebDessin=0; sonDebDessin < saLargeur - sonPas; sonDebDessin+= sonPas)
+	    {
+		repaint();
+		try{Thread.sleep(sonTemps);}
+		catch (InterruptedException telleExcp)
+		    {telleExcp.printStackTrace();}
+	    }
+	for(sonDebDessin=saLargeur;sonDebDessin> sonPas;sonDebDessin-=sonPas ){
+		repaint();
+		try{Thread.sleep(sonTemps);}
+		catch (InterruptedException telleExcp)
+		{telleExcp.printStackTrace();}
+		}
 	}
 
     public void paintComponent(Graphics telCG)
@@ -44,11 +36,4 @@ class UnMobile extends JPanel implements Runnable
 	super.paintComponent(telCG);
 	telCG.fillRect(sonDebDessin, saHauteur/2, sonCote, sonCote);
     }
-	public void arreter() {
-		running = false;
-	}
-
-	public void reprendre() {
-		running = true;
-	}
 }
--- src/UneFenetre.java
+++ src/UneFenetre.java
@@ -20,27 +20,10 @@ class UneFenetre extends JFrame
         this.setVisible(true);
         Container conteneur = getContentPane();
         conteneur.setLayout(new BorderLayout());
-        JButton sonBouton = new JButton("start/stop");
-        sonBouton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                boutonActivation(tache);
-            }
-        });
         conteneur.add(tache);
         this.setSize(LARG, HAUT);
-        conteneur.add(sonBouton,BorderLayout.WEST);
 	// lancer laThread
         supportDeTache.start();
     }
-    private  void boutonActivation(UnMobile tache){
-        if(!stop){
-            tache.arreter();
-            stop = true;
-        }
-        else{
-            stop = false;
-            tache.reprendre();
-        }
-    }
 }
+
