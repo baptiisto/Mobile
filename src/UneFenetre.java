b5d40ad4e740138740309cd32c7cda972f8b1200
baptiisto <baptisto78@gmail.com>
2024-10-11 15:29:30 +0200

    (docs) : Ajout de la conception pour le TP3

--- src/UneFenetre.java
+++ src/UneFenetre.java
@@ -13,7 +13,7 @@ class UneFenetre extends JFrame
         Container conteneur = getContentPane();
         conteneur.setLayout(new BoxLayout(conteneur,BoxLayout.Y_AXIS));
         this.setSize(LARG, HAUT);
-        CemaphoreGeneral cg = new CemaphoreGeneral(1);
+        CemaphoreGeneral cg = new CemaphoreGeneral(5);
 	// ajouter sonMobile a la fenetre
 	// creer une thread laThread avec sonMobile
         for(int e = 0;e < nombre;e++){
